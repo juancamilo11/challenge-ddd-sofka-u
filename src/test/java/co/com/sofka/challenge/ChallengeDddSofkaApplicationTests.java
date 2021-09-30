d5b6a01017745acd4b5343ce8e6cc812ee088dbf
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-30 11:23:22 -0500

    Se añadieron las librerias de DDD de sofka

---
+++ src/test/java/co/com/sofka/challenge/ChallengeDddSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDddSofkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
